31630fb6566a1c8b5d0cbea7edba7456e6e52d16
SSC202 <e22750706642022@163.com>
2023-11-13 17:19:44 +0800

    LVGL 部件

--- Code/4_LVGL_Component/main.c
+++ Code/4_LVGL_Component/main.c
@@ -84,6 +84,39 @@ static void My_Demo_Label_GUI(void)
   lv_obj_set_style_text_font(label, &lv_font_montserrat_14, LV_STATE_DEFAULT);
   lv_obj_set_style_text_color(label, lv_color_hex(0xf7b37b), LV_STATE_DEFAULT);
 }
+
+/**
+ * @brief 自定义CheckBox GUI函数
+ */
+static void My_Demo_CheckBox_GUI(void)
+{
+  lv_obj_t *checkbox = lv_checkbox_create(lv_scr_act());
+  lv_checkbox_set_text(checkbox, "check it!");
+  lv_obj_set_style_pad_column(checkbox, 20, LV_STATE_DEFAULT); // 设置文本和复选框的间距
+}
+
+/**
+ * @brief 自定义Bar GUI函数
+ */
+static void My_Demo_Bar_GUI(void)
+{
+  lv_obj_t *bar = lv_bar_create(lv_scr_act());
+  lv_obj_set_style_anim_time(bar, 500, LV_STATE_DEFAULT); // 设置动画时间
+  lv_bar_set_range(bar, -100, 100);                       // 设置范围值
+  lv_bar_set_value(bar, 50, LV_ANIM_ON);                  // 设置当前值
+}
+
+/**
+ * @brief 自定义Spinner GUI函数
+ */
+static void My_Demo_Spinner_GUI(void)
+{
+  lv_obj_t *spinner = lv_spinner_create(lv_scr_act());
+  lv_obj_set_style_arc_color(spinner, lv_color_hex(0x4a9f00), LV_PART_MAIN);      // 设置主体圆弧颜色
+  lv_obj_set_style_arc_color(spinner, lv_color_hex(0x83bd55), LV_PART_INDICATOR); // 设置指示器圆弧颜色
+  lv_obj_set_style_arc_width(spinner, 30, LV_PART_MAIN);                          // 设置主体圆弧宽度
+  lv_obj_set_style_arc_width(spinner, 30, LV_PART_INDICATOR);                     // 设置指示器圆弧宽度
+}
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
@@ -98,7 +131,9 @@ int main(int argc, char **argv)
   hal_init(800, 480);
   // My_Demo_Label_GUI();
   // My_Demo_Button_GUI();
-  My_Demo_Switch_GUI();
+  // My_Demo_CheckBox_GUI();
+  // My_Demo_Bar_GUI();
+  My_Demo_Spinner_GUI();
 
   while (1)
   {
